0283c338ff19a754f8369459b991511f9c0216ca
K1NGAARON <gerardaaron1997@gmail.com>
2023-10-30 20:15:37 +0100

    added slider

--- home/js/script.js
+++ home/js/script.js
@@ -37,3 +37,19 @@ function changeReviews() {
 };
 
 $('.dot').click(changeReviews);
+
+document.addEventListener( 'DOMContentLoaded', function() {
+    new Splide( '#gallery-slides', {
+        heightRatio: 0.3,
+        perPage: 3,
+        perMove: 1,
+        // breakpoints: {
+        //     1000: {
+        //         heightRatio: 1,
+        //         perPage: 2,
+        //     }
+        // },
+        type: 'loop',
+        speed: 3000,
+    }).mount();
+});
