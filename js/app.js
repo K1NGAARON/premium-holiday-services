a9f1cf146ceec97d97bf0838d361815bda4fdb12
K1NGAARON <gerardaaron1997@gmail.com>
2023-10-30 19:56:35 +0100

    added cookie banner

--- js/app.js
+++ js/app.js
@@ -46,3 +46,26 @@ $(window).scroll(function () {
         return;
     }
 });
+
+const cookieBanner = document.querySelector(".cookie-banner-wrapper");
+const cookiesAccepted = document.querySelector("#accept-cookies");
+const blackOverlay = document.querySelector("#overlay");
+
+cookiesAccepted.addEventListener("click", function acceptCookies() {
+    blackOverlay.classList.add("hide-cookie-banner");
+    cookieBanner.classList.add("hide-cookie-banner");
+    storeCookieConsent();
+});
+
+function storeCookieConsent() {
+    localStorage.cookies = "accepted";
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    if ("cookies" in localStorage) {
+        blackOverlay.classList.add("hide-cookie-banner");
+        cookieBanner.classList.add("hide-cookie-banner");
+    } else {
+        // Shows cookieBanner
+    }
+});
